728cd0715cd46228151cbbb38578c54e70fc5e79
realmade <brandon@realmade.co>
2025-02-15 19:59:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-let navChange = gsap.timeline({
-  scrollTrigger: {
-    trigger: "body",
-    start: "5% 5%",
-    end: "10% top",
-    scrub: true,
-    markers: true,
-  },
-});
 
-navChange.to(".nav_contain.is-desktop", {
-  backgroundColor: "#0B0B0C",
-  boxShadow: "0px 0px 30px rgba(255, 255, 255, 0.05)",
-  duration: 0.5,
-  ease: "circ.out",
-  color: "#FFFFFF",
-});
